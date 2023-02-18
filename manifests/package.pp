e85ad6c9a3fe4026bee1a6de2e7fdd42f8825918
michael-nt <34501742+michael-nt@users.noreply.github.com>
2023-02-18 23:41:01 +0800

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,13 +6,6 @@ package { 'apparmor':
   ensure => absent,
 }
 
-package { 'openssl':
-  ensure => '1.0.2g-1ubuntu4.8',
-}
-
-package { 'ruby':
-  ensure => installed,
-}
 
 package { 'puppet-lint':
   ensure   => installed,
