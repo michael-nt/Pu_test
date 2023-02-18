740e0a2f976b5969ac742d505fd54ccd4321d05a
michael-nt <34501742+michael-nt@users.noreply.github.com>
2023-02-18 21:09:46 +0800

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,3 +1,21 @@
 package {'cowsay':
   ensure => installed,
 }
+
+package { 'apparmor':
+  ensure => absent,
+}
+
+package { 'openssl':
+  ensure => '1.0.2g-1ubuntu4.8',
+}
+
+package { 'ruby':
+  ensure => installed,
+}
+
+package { 'puppet-lint':
+  ensure   => installed,
+  provider => gem,
+}
+
